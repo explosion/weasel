6a246f09c0c54ef08247e07250834ecf6032e180
Vincent <vincentwarmerdam@gmail.com>
2022-11-04 13:11:37 +0100

    assets

--- weasel/__main__.py
+++ weasel/__main__.py
@@ -4,6 +4,7 @@ from .cli.clone import clone_cli
 from .cli.document import document_cli
 from .cli.pull import pull_cli
 from .cli.run import run_cli
+from .cli.assets import assets_cli
 from .info import HELP, NAME
 
 app = typer.Typer(name=NAME, help=HELP, add_completion=False, no_args_is_help=True)
@@ -11,6 +12,7 @@ app.command("clone")(clone_cli)
 app.command("document")(document_cli)
 app.command("pull")(pull_cli)
 app.command("run")(run_cli)
+app.command("assets")(assets_cli)
 
 
 if __name__ == "__main__":
