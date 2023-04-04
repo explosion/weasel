de36ea01ea54d1d25638b2c87813225ef4b92fb6
Basile Dura <bdura@users.noreply.github.com>
2023-04-04 08:57:20 +0200

    feat: update weasel/_util.py wording
    Co-authored-by: Adriane Boyd <adrianeboyd@gmail.com>

--- weasel/_util.py
+++ weasel/_util.py
@@ -213,7 +213,7 @@ def validate_spacy_version(config: Dict[str, Any]) -> None:
             err = (
                 f"The {PROJECT_FILE} specifies a spaCy version range ({version_specifier}) "
                 f"but spaCy is not installed within the environment. "
-                f"Did you forget to add 'spacy{version_specifier}` to your requirement file?"
+                f"Did you forget to add 'spacy{version_specifier}` to your requirements?"
             )
             msg.fail(err, exits=1)
 
