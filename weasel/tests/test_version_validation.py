63415ede7aab3040001565c80f3f79b4cccdc3f7
Basile Dura <basile@bdura.me>
2023-04-20 14:43:39 +0200

    test: adapt tests to the new version validation scheme

--- weasel/tests/test_version_validation.py
+++ weasel/tests/test_version_validation.py
@@ -1,27 +1,39 @@
+from typing import Optional
+
 import pytest
 
-from weasel import _util
+from weasel import util
 from weasel._util import validate_spacy_version
 
 
-def mock_version(package: str) -> str:
-    return "3.4.2"
+def mock_version(output: Optional[str]) -> str:
+    def f(package):
+        return output
+
+    return f
 
 
 @pytest.mark.parametrize(
-    "specifier,should_exit",
+    "version,specifier,should_exit",
     [
-        ("3.4.2", False),
-        (">=3.4.1", False),
-        (">=3.0.1", False),
-        (">3.0", False),
-        (">3.4", False),
-        ("!=3.4.2", True),
-        ("3.5.0", True),
+        ("3.4.2", "3.4.2", False),
+        ("3.4.2", ">=3.4.1", False),
+        ("3.4.2", ">=3.0.1", False),
+        ("3.4.2", ">3.0", False),
+        ("3.4.2", ">3.4", False),
+        ("3.4.2", "!=3.4.2", True),
+        ("3.4.2", "3.5.0", True),
+        (None, "3.4.2", False),
+        (None, ">=3.4.1", False),
+        (None, ">=3.0.1", False),
+        (None, ">3.0", False),
+        (None, ">3.4", False),
+        (None, "!=3.4.2", False),
+        (None, "3.5.0", False),
     ],
 )
-def test_spacy(monkeypatch, specifier: str, should_exit: bool):
-    monkeypatch.setattr(_util, "version", mock_version)
+def test_spacy(monkeypatch, version: Optional[str], specifier: str, should_exit: bool):
+    monkeypatch.setattr(util, "version", mock_version(version))
 
     if should_exit:
         with pytest.raises(SystemExit):
