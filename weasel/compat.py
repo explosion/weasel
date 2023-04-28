5636af386a407ecc4036aa7aa233076baafdacf4
Basile Dura <basile@bdura.me>
2023-04-28 09:55:17 +0200

    feat: remove multiple import since we're dropping Py3.7 support

--- weasel/compat.py
+++ weasel/compat.py
@@ -3,8 +3,3 @@ import sys
 is_windows = sys.platform.startswith("win")
 is_linux = sys.platform.startswith("linux")
 is_osx = sys.platform == "darwin"
-
-try:
-    from importlib.metadata import version  # noqa
-except ImportError:
-    from catalogue._importlib_metadata import version  # type: ignore [no-redef]  # noqa
