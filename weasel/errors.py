4f63971181b0fd4b7556cc0ce0e160c45f041c96
Basile Dura <basile@bdura.me>
2023-05-16 16:17:54 +0200

    feat: space error codes out

--- weasel/errors.py
+++ weasel/errors.py
@@ -9,22 +9,22 @@ class ErrorsWithCodes(type):
 
 class Warnings(metaclass=ErrorsWithCodes):
     # File system
-    W2001 = "Could not clean/remove the temp directory at {dir}: {msg}."
+    W801 = "Could not clean/remove the temp directory at {dir}: {msg}."
 
 
 class Errors(metaclass=ErrorsWithCodes):
     # API - Datastructure
-    E0001 = (
+    E001 = (
         "Can't write to frozen dictionary. This is likely an internal "
         "error. Are you writing to a default function argument?"
     )
-    E0002 = (
+    E002 = (
         "Can't write to frozen list. Maybe you're trying to modify a computed "
         "property or default function argument?"
     )
 
     # Workflow
-    E1001 = "Can not execute command '{str_command}'. Do you have '{tool}' installed?"
+    E501 = "Can not execute command '{str_command}'. Do you have '{tool}' installed?"
 
     # File system
-    E2001 = "The tar file pulled from the remote attempted an unsafe path " "traversal."
+    E801 = "The tar file pulled from the remote attempted an unsafe path " "traversal."
