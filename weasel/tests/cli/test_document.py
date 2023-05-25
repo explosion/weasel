24af544d24e7b184e56fe7a2d392095ac2998676
Basile Dura <basile@bdura.me>
2023-05-25 13:35:20 +0200

    test: make test_document rely on dict config

--- weasel/tests/cli/test_document.py
+++ weasel/tests/cli/test_document.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Any, Dict
 
 import pytest
 import srsly
@@ -6,27 +7,52 @@ from typer.testing import CliRunner
 
 from weasel import app
 
-BASE_PATH = Path(__file__).parent.parent
-
 runner = CliRunner()
 
-project_files = [
-    "assets/project_files/ner_demo/project.yml",
-    "assets/project_files/ner_drugs/project.yml",
-]
+SAMPLE_PROJECT: Dict[str, Any] = {
+    "title": "Sample project",
+    "description": "This is a project for testing",
+    "assets": [
+        {
+            "dest": "assets/weasel-readme.md",
+            "url": "https://github.com/explosion/weasel/raw/9a3632862b47069d2f9033b773e814d4c4e09c83/README.md",
+            "checksum": "65f4c426a9b153b7683738c92d0d20f9",
+        },
+        {
+            "dest": "assets/pyproject.toml",
+            "url": "https://github.com/explosion/weasel/raw/9a3632862b47069d2f9033b773e814d4c4e09c83/pyproject.toml",
+            "checksum": "1e2da3a3030d6611520952d5322cd94e",
+            "extra": True,
+        },
+    ],
+    "commands": [
+        {
+            "name": "ok",
+            "help": "print ok",
+            "script": ["python -c \"print('okokok')\""],
+        },
+        {
+            "name": "create",
+            "help": "make a file",
+            "script": ["touch abc.txt"],
+            "outputs": ["abc.txt"],
+        },
+        {
+            "name": "clean",
+            "help": "remove test file",
+            "script": ["rm abc.txt"],
+        },
+    ],
+}
 
 
-@pytest.fixture(scope="function", params=project_files)
+@pytest.fixture(scope="function")
 def project_yaml_file(
-    request: pytest.FixtureRequest,
     tmp_path_factory: pytest.TempPathFactory,
 ):
-    full_path = BASE_PATH / Path(request.param)
-    config_contents = full_path.read_text()
-
     test_dir = tmp_path_factory.mktemp("project")
     path = test_dir / "project.yml"
-    path.write_text(config_contents)
+    path.write_text(srsly.yaml_dumps(SAMPLE_PROJECT))
     return path
 
 
